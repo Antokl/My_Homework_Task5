731b2b878288e6776c4ce9845a7c3f847f8f87da
Antokl <liantokha@mail.ru>
2024-09-02 18:18:22 +0300

    Add project files.

---
+++ My_Homework_Task№5.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime> 
+using namespace std;
+
+int main() {
+
+    const int ar = 4;
+
+    int array[ar][ar];
+
+    for (int i = 0; i < ar; i++) {
+        for (int j = 0; j < ar; j++) {
+            array[i][j] = i + j;
+        }
+    }
+
+    for (int i = 0; i < ar; i++) {
+        for (int j = 0; j < ar; j++) {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
